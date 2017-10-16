f6d3b090b44f25a7dd1a6ad7a4fef0a527ae0d19
Martin Berggren <martin@digilar.se>
2017-10-16 23:52:47 +0200

    Styling!

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
+import RouteQueryManager from "ember-apollo-client/mixins/route-query-manager";
+import query from "shareable-ember/gql/queries/currentUser";
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(RouteQueryManager, {
+
+  model() {
+    return this.get('apollo').query({ query }, 'user').catch(error => alert(error));
+  },
   actions: {
     navigateHome() {
       this.transitionTo('/');
